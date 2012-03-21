66c9ec64184a54c75caaf020b75f9fe28c6fba1c
Roland Tritsch <roland@tritsch.org>
2012-03-21 13:25:11 +0000

    Adding dependency on org.apache.--- to build.sbt

---
+++ roland/build.sbt
@@ -0,0 +1,7 @@
+name := "hello"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.1.2" 
---
+++ roland/src/main/scala/Hello.scala
@@ -0,0 +1,9 @@
+package org.tritsch.scala.hello
+
+import org.apache.http.client.HttpClient
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
